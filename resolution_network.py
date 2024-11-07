e55991a1b801c480570500a056dc3d2b7a9d5cb1
Gyan Singh Budhiraja <92247410+Gyan-Singh@users.noreply.github.com>
2024-11-07 11:24:39 +0530

    Update resolution_network.py

--- resolution_network.py
+++ resolution_network.py
@@ -7,7 +7,8 @@ https://arxiv.org/abs/1811.00656
 from tf_utils import utils as tfutils
 import tensorflow as tf
 import tf_slim as slim
-from tensorflow.contrib.slim.python.slim.nets import resnet_v1, vgg
+from tf_slim.nets import resnet_v1, vgg
+
 
 
 class ResoNet(object):
